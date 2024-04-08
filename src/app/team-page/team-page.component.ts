e5f1470b26fa3673b3bedafbd432f0e8b3857817
Lisi4ka <0059nai@gmail.com>
2024-04-08 16:07:33 +0300

    create team cards with all styles

--- src/app/team-page/team-page.component.ts
+++ src/app/team-page/team-page.component.ts
@@ -1,15 +1,72 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ScrollPanelModule} from "primeng/scrollpanel";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {NgOptimizedImage} from "@angular/common";
+import {CarouselModule} from "primeng/carousel";
+import {TagModule} from "primeng/tag";
 
 @Component({
   selector: 'app-team-page',
   standalone: true,
   imports: [
-    ScrollPanelModule
+    ScrollPanelModule,
+    CardModule,
+    ButtonModule,
+    NgOptimizedImage,
+    CarouselModule,
+    TagModule
   ],
   templateUrl: './team-page.component.html',
   styleUrl: './team-page.component.css'
 })
 export class TeamPageComponent {
-
+  items: CarouselItems[] = [
+    {
+      id: 1,
+      name: 'Дмитрий Матвеев',
+      info: 'Lorem ipsum dolor sit amet, consectetur',
+      description: 'Lorem ipsum dolor sit amet, consectetur',
+      telegram: '@SloggerD',
+      telegramLink: 'https://t.me/SloggerD',
+      phone: '+7 (922) 366-51-38',
+      phoneLink: 'tel:+79223665138',
+      image: 'assets/team_icon.png'
+    },
+    {
+      id: 2,
+      name: 'Дмитрий Матвеев',
+      info: 'Lorem ipsum dolor sit amet, consectetur',
+      description: 'Lorem ipsum dolor sit amet, consectetur',
+      telegram: '@SloggerD',
+      telegramLink: 'https://t.me/SloggerD',
+      phone: '+7 (922) 366-51-38',
+      phoneLink: 'tel:+79223665138',
+      image: 'assets/team_icon.png'
+    },
+    {
+      id: 3,
+      name: 'Дмитрий Матвеев',
+      info: 'Lorem ipsum dolor sit amet, consectetur',
+      description: 'Lorem ipsum dolor sit amet, consectetur',
+      telegram: '@SloggerD',
+      telegramLink: 'https://t.me/SloggerD',
+      phone: '+7 (922) 366-51-38',
+      phoneLink: 'tel:+79223665138',
+      image: 'assets/team_icon.png'
+    },
+  ];
 }
+export interface CarouselItems {
+  id?: number;
+  name?: string;
+  info?: string;
+  description?: string;
+  telegram?: string;
+  telegramLink?: string;
+  phone?: string;
+  phoneLink?: string;
+  image?: string;
+}
+
+
